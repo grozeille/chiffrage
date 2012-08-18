facd5a21e4c7ebae672344ad277b52517bb85254
Mathias Kluba <mathias.kluba@gmail.com>
2012-08-18 15:42:46 +0200

    small changes: rename Chiffrage.Tests by Chiffrage.Mvc.Tests

---
+++ Chiffrage.Mvc.Tests/EventBrokerTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Threading;
+using Chiffrage.Mvc.Events;
+using NUnit.Framework;
+using Moq;
+
+namespace Chiffrage.Mvc.Tests
+{
+    [TestFixture]
+    public class EventBrokerTests
+    {
+        public class MyEvent : IEvent
+        {
+        }
+
+        public class MySubEvent : MyEvent
+        {
+        }
+
+        public abstract class MyEventHandler : IGenericEventHandler<MyEvent>, IGenericEventHandler<MySubEvent>
+        {
+            #region IGenericEventHandler<MyEvent> Members
+
+            public abstract void ProcessAction(MyEvent action);
+
+            #endregion
+
+            #region IGenericEventHandler<MySubEvent> Members
+
+            public abstract void ProcessAction(MySubEvent action);
+
+            #endregion
+        }
+
+        [Test]
+        public void TestWithExactType()
+        {
+            var eventBroker = new EventBroker();
+
+            var eventHandler = new Mock<IGenericEventHandler<MyEvent>>();
+
+            eventBroker.Subscribers = new IEventHandler[] {eventHandler.Object};
+
+            eventBroker.Start();
+
+            eventBroker.Publish(new MyEvent());
+
+            Thread.Sleep(3*1000);
+
+            eventHandler.Verify(x => x.ProcessAction(It.IsAny<MyEvent>()));
+        }
+
+        [Test]
+        public void TestWithMultipleType()
+        {
+            var eventBroker = new EventBroker();
+
+            var eventHandler = new Mock<MyEventHandler>();
+
+            eventBroker.Subscribers = new IEventHandler[] {eventHandler.Object};
+
+            eventBroker.Start();
+
+            eventBroker.Publish(new MySubEvent());
+
+            Thread.Sleep(3*1000);
+
+            eventHandler.Verify(x => x.ProcessAction(It.IsAny<MyEvent>()));
+
+            eventHandler.Verify(x => x.ProcessAction(It.IsAny<MySubEvent>()));            
+        }
+
+        [Test]
+        public void TestWithSubType()
+        {
+            var eventBroker = new EventBroker();
+
+            var eventHandler = new Mock<IGenericEventHandler<MyEvent>>();
+
+            eventBroker.Subscribers = new IEventHandler[] { eventHandler.Object };
+
+            eventBroker.Start();
+
+            eventBroker.Publish(new MySubEvent());
+
+            Thread.Sleep(3*1000);
+
+            eventHandler.Verify(x => x.ProcessAction(It.IsAny<MyEvent>()));
+        }
+    }
+}
